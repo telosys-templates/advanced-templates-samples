53c955721e2c9a0806b7e3fdf270e321cc32f1bb
l-gu <laurent@telosys.org>
2023-08-17 17:42:50 +0200

    .class files compilation with Java ver 1.8

--- classes/compile.sh
+++ classes/compile.sh
@@ -1,3 +1,3 @@
-javac MyData.java  
-javac org/foo/FooData.java
-javac -cp ../lib/*.jar  org/foo/MyFooClass.java
+javac -source 1.8 -target 1.8 MyData.java  
+javac -source 1.8 -target 1.8 org/foo/FooData.java
+javac -source 1.8 -target 1.8 -cp ../lib/*.jar  org/foo/MyFooClass.java
